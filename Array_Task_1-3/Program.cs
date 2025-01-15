9103a0d32f813085dd969ba3e268b322e0e31916
Elkhan Islamov <Elkhanmi@code.edu.az>
2025-01-15 16:53:22 +0400

    first commit

---
+++ Array_Task_1-3/Program.cs
@@ -0,0 +1,28 @@
+﻿//1.Verilmiş tam ədədlərdən ibarət massivdəki mənfi ədədləri müsbət ədədlərlə əvəz edin.
+
+//Giriş: -1, 2, -3, 4, -5
+//Çıxış: 1, 2, 3, 4, 5
+
+//int[] numbers = { -1, -2, -3, -4, -5 };
+//for (int i = 0;i<numbers.Length;i++)
+//{
+//    int result = -1;
+//    result*= numbers[i];
+//    Console.Write(result);
+//}
+
+//  2. Verilmiş massivdə 0-ı başa gətirən yeni massiv yaradın.
+
+//Giriş: 1, 0, 2, 0, 3
+//Çıxış: 0, 0, 1, 2, 3
+
+//int[] arr = { 1, 0, 2, 0, 3 };
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] == 0)
+//        Console.Write(arr[i] + " ");
+      
+//}
+
+
